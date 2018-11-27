aa089d283abcc69c6eb727e2b521b7ef8454828b
Nithin Bose <nithinbose@gmail.com>
2018-11-27 16:20:43 +0530

    added the deploy all command

--- cmd/k8s/deploy/all/root.go
+++ cmd/k8s/deploy/all/root.go
@@ -1,8 +1,6 @@
-package deploy
+package all
 
 import (
-	"deployer/cmd/k8s/deploy/all"
-
 	"github.com/spf13/cobra"
 )
 
@@ -10,10 +8,9 @@ var chartsDir string
 
 func init() {
 	RootCmd.PersistentFlags().StringVarP(&chartsDir, "charts-dir", "d", "", "Path where the charts directory is present")
-	RootCmd.AddCommand(all.RootCmd)
 }
 
 var RootCmd = &cobra.Command{
-	Use:   "deploy",
-	Short: "Deployment tasks. Helm is required to be installed and configured",
+	Use:   "all",
+	Short: "Deploy all charts",
 }
